30e274a8f9f733e38c096f6ae7cc674e67689103
danielamtzrgz <135908380+danielamtzrgz@users.noreply.github.com>
2023-06-12 13:17:34 -0600

    Add files via upload

---
+++ codigo_preguntas.R
@@ -0,0 +1,12 @@
+# Segunda fase INTERSECTA 
+
+  # Paquetes 
+
+## Pregunta 1 
+
+enpol21s <- list.files('Bases de datos/01_bd_enpol_2021_csv')
+enpol21 <- lapply(enpol21s, function(x) read.csv(paste('Bases de datos/01_bd_enpol_2021_csv',x,sep="/")))
+
+## Pregunta 2
+
+## Pregunta 3 
